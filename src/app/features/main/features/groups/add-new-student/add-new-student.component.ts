52312c858be5b9dd872a89fdf1893851b1695d5e
Yuriy Bezgachnyuk <yurkovskiy@gmail.com>
2017-11-07 19:22:28 +0200

    lesson 7.1

--- src/app/features/main/features/groups/add-new-student/add-new-student.component.ts
+++ src/app/features/main/features/groups/add-new-student/add-new-student.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-new-student',
@@ -6,10 +7,47 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-new-student.component.scss']
 })
 export class AddNewStudentComponent implements OnInit {
+  addresses = [];
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      email: {value: null, disabled: false}, // ['test', Validators.email],
+      password: [null, Validators.required]
+    });
+
+    // this.form.valueChanges.subscribe(data => {
+    //   console.log(data);
+    // });
+    this.form.get('password').valueChanges.subscribe(data => {
+      this.form.get('email').reset();
+    });
+
+    // this.form = new FormGroup({
+    //   email: new FormControl(
+    //     'test',
+    //     Validators.email
+    //   ),
+    //   password: new FormControl('123456')
+    // });
+  }
+
+  makeEnabled(): void {
+    this.form.get('email').enable();
+  }
+
+  setDefault(): void {
+    // this.form.patchValue({
+    //   password: '12345'
+    // });
+    this.form.get('email').setValue('test@test');
+  }
+
+  saveData(): void {
+    console.log(this.form.value);
+    this.form.reset();
   }
 
 }
